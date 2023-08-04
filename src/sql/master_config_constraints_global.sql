a0872a71df7952692f5dc45b5e5732a4be7a1898
Jason Chang <jasonchang1712@gmail.com>
2023-08-03 21:11:42 -0500

    Add Text Message spend to GOV clients.

--- src/sql/master_config_constraints_global.sql
+++ src/sql/master_config_constraints_global.sql
@@ -15,7 +15,7 @@ values
     --  INCREMENT: RUNNING LEVEL ONLY.
     ('MAX_COST_RUNNING_TOTAL',          26000),
     ('MAX_COST_RUNNING_LETTERS',        7500),
-    ('MAX_COST_RUNNING_TEXTS',          2850),
+    ('MAX_COST_RUNNING_TEXTS',          3400),
     ('MAX_COST_RUNNING_VOAPPS',         21000),
     ('MAX_COST_RUNNING_EMAILS',         0),
     
